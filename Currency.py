903ecdea7b2f2f08f4f30ed6e2da6dd355aae869
manuvasro-sys <manuvasro@gmail.com>
2026-06-20 08:43:07 -0600

    Update and rename README.md to Currency.py

---
+++ Currency.py
@@ -0,0 +1,7 @@
+# Python-Practices
+Codédex Currency Practice
+co = int(input('What do you have left in pesos? ')) 
+pe = int(input('What do you have left in soles? '))
+br = int(input('What do you have left in reais? '))
+total_usd = (co * 0.00029) + (pe * 0.30) + (br * 0.19)
+print(total_usd)
--- README.md
+++
@@ -1,2 +0,0 @@
-# Python-Practices
-Codédex Currency Practice
